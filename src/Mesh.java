58501e05a901d245062f02a66dc6173cc2bce535
Kunal Bansal <kunal.b.2003@gmail.com>
2023-05-12 22:52:28 -0400

    Update Mesh.java

--- src/Mesh.java
+++ src/Mesh.java
@@ -1,5 +1,28 @@
 package src;
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
 public class Mesh {
+    private int vertexArrayObject;
+    private int vertexBufferObject;
+
+    private int vertexCount;
+
+    public Mesh() {
+    }
+
+    public boolean create(float vertices[]) {
+        vertexArrayObject = glGenVertexArrays();
+        glBindVertexArray(vertexArrayObject);
+        
+        vertexBufferObject = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+
     
+        glBindVertexArray(0);
+        return true;
+    }
 }
