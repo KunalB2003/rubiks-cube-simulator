848d70c6df17e366336034d5c02a90cea99e64d0
Kunal Bansal <kunal.b.2003@gmail.com>
2023-05-13 00:39:14 -0400

    implement shaders and transforms into existing triangle demo

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,28 @@ package src;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 
+import org.joml.AxisAngle4f;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+import src.graphics.Camera;
 import src.graphics.Mesh;
 import src.graphics.Shader;
+import src.graphics.Transform;
 import src.io.Window;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int width = 640;
+        int height = 480;
+
         Window window = new Window();
 
-        window.createWindow("Window", 640, 480);
+        window.createWindow("Window", width, height);
 
-        Mesh testMesh = new Mesh();
-        testMesh.create(new float[] {
+        Mesh mesh = new Mesh();
+        mesh.create(new float[] {
                 -1, -1, 0,
                 0, 1, 0,
                 1, -1, 0
@@ -24,26 +33,36 @@ public class Main {
         Shader shader = new Shader();
         shader.create("basic");
 
+        Camera camera = new Camera();
+        Transform transform = new Transform();
+
+        camera.setPerspective((float) Math.toRadians(70), (float) ((width * 1.0) / (height * 1.0)), 0.01f, 1000.0f);
+        camera.setPosition(new Vector3f(0, 1, 3));
+        camera.setRotation(new Quaternionf(new AxisAngle4f((float) Math.toRadians(-30), new Vector3f(1, 0, 0))));
+
         boolean isRunning = true;
+        float x = 0;
 
         while (isRunning) {
             isRunning = !window.update();
 
+            x++;
+            transform.setPosition(new Vector3f((float) Math.sin(Math.toRadians(x)), 0, 0));
+            transform.getRotation().rotateAxis((float) Math.toRadians(1), 0, 1, 0);
+
             glClear(GL_COLOR_BUFFER_BIT);
 
             shader.useShader();
-            testMesh.draw();
+            shader.setCamera(camera);
+            shader.setTransform(transform);
+            mesh.draw();
 
             window.swapBuffers();
 
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Thread.sleep(1);
         }
 
-        testMesh.destroy();
+        mesh.destroy();
         shader.destroy();
 
         window.free();
