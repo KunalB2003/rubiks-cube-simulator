dc8cd500b58457a109f575cb6c1e9eb97055d39c
Kunal Bansal <kunal.b.2003@gmail.com>
2023-05-17 21:34:40 -0400

    rewrite chat main
    Co-authored-by: Wingo206 <Wingo206@users.noreply.github.com>

--- src/Run.java
+++ src/Run.java
@@ -7,7 +7,7 @@ public class Run {
     public static void main(String[] args) {
         switch (args[0]) {
             case "text":
-                Cube.main(args);
+                Text.main(args);
                 break;
             case "engine":
                 Engine.main(args);
